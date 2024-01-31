f3e206c867e941ee8203593e69f2ca651e12e933
cuu-vnpt <kunnobita97@gmail.com>
2024-01-31 21:59:16 +0700

    newfile

---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace ShapesExample
+{
+    class Shape
+    {
+        private string color;
+
+        public Shape(string color)
+        {
+            this.color = color;
+        }
+
+        public string getColor()
+        {
+            return color;
+        }
+
+        public void setColor(string color)
+        {
+            this.color = color;
+        }
+
+        public override string ToString()
+        {
+            return "Color: " + color;
+        }
+    }
+
+    class Circle : Shape
+    {
+        private double radius;
+
+        public Circle(double radius, string color) : base(color)
+        {
+            this.radius = radius;
+        }
+
+        public double getRadius()
+        {
+            return radius;
+        }
+
+        public void setRadius(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public double getArea()
+        {
+            return Math.PI * radius * radius;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + ", Radius: " + radius + ", Area: " + getArea();
+        }
+    }
+
+    class Cylinder : Circle
+    {
+        private double height;
+
+        public Cylinder(double radius, string color, double height) : base(radius, color)
+        {
+            this.height = height;
+        }
+
+        public double getHeight()
+        {
+            return height;
+        }
+
+        public void setHeight(double height)
+        {
+            this.height = height;
+        }
+
+        public double getVolume()
+        {
+            return getArea() * height;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + ", Height: " + height + ", Volume: " + getVolume();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle(5.0, "Red");
+            Cylinder cylinder = new Cylinder(3.0, "Blue", 10.0);
+
+            Console.WriteLine("Circle: " + circle);
+            Console.WriteLine("Cylinder: " + cylinder);
+        }
+    }
+}
